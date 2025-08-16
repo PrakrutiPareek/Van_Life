c5c5ad0b23fd531a6f369aa9e0a3b40c075181b1
PrakrutiPareek <prakruti.kapadia@gmail.com>
2025-08-16 01:08:13 +0100

    Update About.jsx

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -10,7 +10,8 @@ function About() {
         <p>
           Our mission is to enliven your road trip with the perfect travel van
           rental. Our vans are recertified before each trip to ensure your
-          travel plans can go off without a hitch. (Hitch costs extra 😉) <br />
+          travel plans can go off without a hitch. (Hitch costs extra 😉) <br />{" "}
+          <br />
           Our team is full of vanlife enthusiasts who know firsthand the magic
           of touring the world on 4 wheels.
         </p>
