0306a4fa73b2550a51dea7cc6940e7d10398fde2
PrakrutiPareek <prakruti.kapadia@gmail.com>
2025-09-14 01:21:45 +0100

    final stage 1

--- src/api.js
+++ src/api.js
@@ -1,29 +1,54 @@
-export async function getVans(id) {
-  const url = id ? `/api/vans/${id}` : "/api/vans";
-  const res = await fetch(url);
-  if (!res.ok) {
-    throw {
-      message: "Failed to fetch vans",
-      statusText: res.statusText,
-      status: res.status,
-    };
-  }
-  const data = await res.json();
-  return data.vans;
+import { initializeApp } from "firebase/app";
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  getFirestore,
+  query,
+  where,
+} from "firebase/firestore/lite";
+const firebaseConfig = {
+  apiKey: "AIzaSyAK3W_R3qRM1dE_iOnleK22yG0WySNqWRk",
+  authDomain: "vanlife-c1c69.firebaseapp.com",
+  projectId: "vanlife-c1c69",
+  storageBucket: "vanlife-c1c69.firebasestorage.app",
+  messagingSenderId: "693721263517",
+  appId: "1:693721263517:web:49eb1e21dbe40407d25374",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const vanCollectionRef = collection(db, "vans");
+
+export async function getVans() {
+  const querySnapshot = await getDocs(vanCollectionRef);
+  const dataArr = querySnapshot.docs.map((doc) => ({
+    ...doc.data(),
+    id: doc.id,
+  }));
+  return dataArr;
 }
 
-export async function getHostVans(id) {
-  const url = id ? `/api/host/vans/${id}` : "/api/host/vans";
-  const res = await fetch(url);
-  if (!res.ok) {
-    throw {
-      message: "Failed to fetch vans",
-      statusText: res.statusText,
-      status: res.status,
-    };
-  }
-  const data = await res.json();
-  return data.vans;
+export async function getVan(id) {
+  const docRef = doc(db, "vans", id);
+  const vanSnapshot = await getDoc(docRef);
+  return {
+    ...vanSnapshot.data(),
+    id: vanSnapshot.id,
+  };
+}
+
+export async function getHostVans() {
+  const q = query(vanCollectionRef, where("hostId", "==", "123"));
+  const querySnapshot = await getDocs(q);
+  const dataArr = querySnapshot.docs.map((doc) => ({
+    ...doc.data(),
+    id: doc.id,
+  }));
+  console.log(dataArr);
+  return dataArr;
 }
 
 export async function loginUser(creds) {
